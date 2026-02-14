59da995a5dd442870331ce365ae5a840310c3a0b
arsenevaslin12-png <arsenevaslin12@gmail.com>
2026-02-15 00:39:40 +0100

    moteur de calcul et surcharge progressive

---
+++ api/workout.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+    res.status(200).json({
+        title: "SÉANCE ADAPTATIVE",
+        description: "Objectif : 11 répétitions. (Surcharge progressive activée)"
+    });
+}
